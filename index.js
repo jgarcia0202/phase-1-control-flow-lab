a9bbabfae2fe35d87d5e51e41eb9765378e0ec34
Jose <jmgarcia0202@gmail.com>
2021-10-02 21:05:10 -0700

    Completed Assignment

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
-function scuberGreetingForFeet(){
   // Write your code here!
+function scuberGreetingForFeet(someValue) {
+  if (someValue <= 400) {
+      return 'This one is on me!'
+    }
+  else if (someValue <=2500) {
+      return 'I will gladly take your thirty bucks.'
+    }
+  else if (someValue > 2500) {
+      return 'No can do.'
+    }
+  }
+
+function ternaryCheckCity(city){
+  // Write your code here!
+  if (city === 'NYC') {
+    return "Ok, sounds good."
+  }
+  else {
+    return 'No go.'
+  }
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  if (tip === 'generous') {
+    return "Thank you so much."
+  }
+  else if (tip === 'not as generous') {
+    return "Thank you."
+  }
+  else {
+    return 'Bye.'
+  }
 }
-
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+scuberGreetingForFeet(199);
+scuberGreetingForFeet(2001);
+scuberGreetingForFeet(2501);
+ternaryCheckCity('NYC');
+ternaryCheckCity('Pittsburgh');
+switchOnCharmFromTip('generous');
+switchOnCharmFromTip('not as generous');
+switchOnCharmFromTip(2);
