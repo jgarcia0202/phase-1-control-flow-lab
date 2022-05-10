d1ead6c7554ec142cdec5b44d5306c23f84ae87e
Jose <jmgarcia0202@gmail.com>
2022-05-10 11:04:00 -0700

    redone

--- index.js
+++ index.js
@@ -1,43 +1,35 @@
+// Write your code in this file!
+function scuberGreetingForFeet(x){
   // Write your code here!
-function scuberGreetingForFeet(someValue) {
-  if (someValue <= 400) {
-      return 'This one is on me!'
-    }
-  else if (someValue <=2500) {
-      return 'I will gladly take your thirty bucks.'
-    }
-  else if (someValue > 2500) {
-      return 'No can do.'
-    }
+  if (x <= 400) {
+    return 'This one is on me!'
   }
-
-function ternaryCheckCity(city){
+  else if (x > 2500) {
+    return 'No can do.'
+  }
+  else if (x > 2000) {
+    return 'I will gladly take your thirty bucks.'
+  }
+}
+function ternaryCheckCity(a){
   // Write your code here!
-  if (city === 'NYC') {
-    return "Ok, sounds good."
+  if (a === 'NYC') {
+    return 'Ok, sounds good.'
   }
   else {
     return 'No go.'
   }
 }
 
-function switchOnCharmFromTip(tip){
+function switchOnCharmFromTip(b){
   // Write your code here!
-  if (tip === 'generous') {
-    return "Thank you so much."
+  if (b === 'generous') {
+    return 'Thank you so much.'
   }
-  else if (tip === 'not as generous') {
-    return "Thank you."
+  else if (b === 'not as generous') {
+    return 'Thank you.'
   }
   else {
     return 'Bye.'
   }
 }
-scuberGreetingForFeet(199);
-scuberGreetingForFeet(2001);
-scuberGreetingForFeet(2501);
-ternaryCheckCity('NYC');
-ternaryCheckCity('Pittsburgh');
-switchOnCharmFromTip('generous');
-switchOnCharmFromTip('not as generous');
-switchOnCharmFromTip(2);
